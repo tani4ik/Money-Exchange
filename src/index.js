c8c1162251596b5e2d64fe954f594d6a6456f4bf
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-27 14:11:58 +0300

    Implemented money exchange task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let coins = [{symbol: 'H', value: 50}, {symbol: 'Q', value: 25}, {symbol: 'D', value: 10}, {symbol: 'N', value: 5}, {symbol: 'P', value: 1}];
+    let result = {};
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    for (let i = 0; i < coins.length; i++) {
+        let division = Math.floor(currency / coins[i].value);
+        if (division >= 1) {
+        	let coinSymbol = coins[i].symbol;
+            currency = currency - division * coins[i].value;
+            result[coinSymbol] = division
+        }
+    }
+    return result;
 }
